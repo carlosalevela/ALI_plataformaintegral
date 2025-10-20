0c770bcfc4634fec2e40c6018a400d03b3bf94d4
Carlos Velasco <carlosvelasco21347@gmail.com>
2025-10-20 16:50:29 -0500

    cambios y cambios mas cambios

--- ali_backend/ali_backend/__init__.py
+++ ali_backend/ali_backend/__init__.py
@@ -1,6 +1,4 @@
-# ali_backend/__init__.py
-import os
-
-# Solo importa Celery si NO está desactivado por env var
-if os.environ.get("DISABLE_CELERY_IMPORT", "0") not in ("1", "true", "True"):
+try:
     from .celery import app as celery_app  # noqa
+except Exception:
+    celery_app = None
